22dfc6ae88ad74e62015bc771455a5a95d26c436
Javier Sayago <javier.sayago000@gmail.com>
2025-09-14 18:09:36 +0000

    verifica porque en el perfil de adminstardor no aparece su menu desplega

--- corp-cami-14-9-2024/src/components/layout/AppLayout.tsx
+++ corp-cami-14-9-2024/src/components/layout/AppLayout.tsx
@@ -57,7 +57,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen w-full">
       {renderSidebar()}
-      <div className="flex flex-col flex-1 md:ml-64">
+      <div className="flex flex-col w-full md:ml-64">
         <Header />
         <main className="p-4 sm:p-6 lg:p-8 flex-1 overflow-auto">{children}</main>
       </div>
