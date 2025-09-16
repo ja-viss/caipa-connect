b18db1c5b4f6269ed43fdad1e472b35b8e0d27d9
Javier Sayago <javier.sayago000@gmail.com>
2025-09-16 23:40:31 +0000

    necesito que elimines el header que contiene el icono del perfil del usu

--- Users/user/github/corp-cami-14-9-2024/src/components/layout/AppLayout.tsx
+++ Users/user/github/corp-cami-14-9-2024/src/components/layout/AppLayout.tsx
@@ -15,7 +15,6 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const [session, setSession] = useState<{ user: User } | null>(null);
   const [loading, setLoading] = useState(true);
-  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
 
   useEffect(() => {
     getSession().then((sessionData) => {
@@ -56,24 +55,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen w-full">
       {renderSidebar()}
-      <div 
-        className="flex flex-col w-full md:ml-64 relative"
-        onMouseLeave={() => setIsHeaderVisible(false)}
-      >
-        <div 
-            className="absolute top-0 left-0 right-0 h-8 z-40"
-            onMouseEnter={() => setIsHeaderVisible(true)}
-        />
-        <Header isVisible={isHeaderVisible} />
-        <main className={cn(
-            "p-4 sm:p-6 lg:p-8 flex-1 transition-all duration-300",
-            isHeaderVisible ? 'pt-20' : 'pt-4'
-          )}
-        >
+      <div className="flex flex-col w-full md:ml-64 relative">
+        <Header />
+        <main className="p-4 sm:p-6 lg:p-8 flex-1">
           {children}
         </main>
       </div>
     </div>
   );
 }
-
