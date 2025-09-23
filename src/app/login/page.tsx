86ac1cc8e41a8e35fa58b07d93566b50d3d9c254
Javier Sayago <javier.sayago000@gmail.com>
2025-09-23 20:40:58 +0000

    cambia la imagen del login por la imagen que esta en la carpeta public/l

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,11 +9,10 @@ export default function LoginPage() {
       </div>
       <div className="hidden bg-muted lg:block">
         <Image
-          src="https://picsum.photos/seed/happy-kids-login-2/1200/800"
+          src="/loggin.webp"
           alt="Imagen de niños felices aprendiendo en un ambiente de apoyo"
           width="1920"
           height="1080"
-          data-ai-hint="happy children learning"
           className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
