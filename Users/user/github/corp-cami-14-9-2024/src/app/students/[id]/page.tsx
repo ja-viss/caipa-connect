fdf3ebef136f0dda4f17428c1cdae2a02e93998c
Javier Sayago <javier.sayago000@gmail.com>
2025-09-23 15:00:05 +0000

    entonces has que la aplicacion trabaje con una ruta y aplica los cmabios

--- Users/user/github/corp-cami-14-9-2024/src/app/students/[id]/page.tsx
+++ Users/user/github/corp-cami-14-9-2024/src/app/students/[id]/page.tsx
@@ -1,3 +1,4 @@
+
 import { getStudentById, getActivityLogsByStudentId, getProgressReportsByStudentId } from '@/lib/actions/students';
 import { notFound } from 'next/navigation';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
